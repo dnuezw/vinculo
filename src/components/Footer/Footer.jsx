07bc64dc5354f3b9385bd2586ab928b5690c349e
Daniel Nuez <daniel.nuez@lifullconnect.com>
2025-05-24 12:07:24 +0200

    Update spotify icon to an official one

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import spotifyIcon from '../../assets/images/spotifyIcon.png'
+import spotifyIcon from '../../assets/icons/spotify.svg'
 
 export default function Footer() {
     return (
         <footer>
             <a href="https://open.spotify.com/" target="_blank" className="myButton">
                 <span>Listen to us on Spotify</span>
-                <img src={spotifyIcon} className="icon" />
+                <img src={spotifyIcon} width={24} height={24} />
             </a>
         </footer>
     );
