caa4da1bd9840772388b14662c5a1aae969b8ccf
dnuez <dnuezw@gmail.com>
2025-05-27 20:02:06 +0200

    add all components

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,30 @@ import Footer from './components/Footer/Footer'
 import Wiki from './components/Wiki/Wiki'
 import { ROUTES_TO_PAGES } from './components/Header/constants'
 import { useState } from 'react'
+import Music from './components/Music/Music'
+import AboutUs from './components/AboutUs/AboutUs'
+import Contact from './components/Contact/Contact'
 
 export default function App() {
   const [location, setLocation] = useState('/');
+
+  function loadPage() {
+    if(location === '/') {
+      return <Presentation />
+    }
+    if(location === '/wiki') {
+      return <Wiki />
+    }
+    if(location === '/music') {
+      return <Music />
+    }
+    if(location === '/about-us') {
+      return <AboutUs />
+    }
+    if(location === '/contact') {
+      return <Contact />
+    }
+  }
 
   return (
     <div className="page">
@@ -21,7 +42,7 @@ export default function App() {
         </div>
       </header>
       <main>
-        {location === '/' ? <Presentation /> : <Wiki />}
+        {loadPage()}
       </main>
       <Footer />
     </div>
